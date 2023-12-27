9e7743c8e99fc0524fffb6e5a4b1635c65509537
spogoretskyi <spogoretskyi@gmail.com>
2023-12-27 22:50:00 +0200

    implementation

--- main.py
+++ main.py
@@ -10,18 +10,86 @@ def generate_unique_numbers(n):
 
 
 # Timsort сортування
-def tim_sort_sorted(array):
+def timsort_sorted(array):
     return sorted(array)
 
 
-def tim_sort_sort(array):
+def timsort_sort(array):
     return array.sort()
 
 
-numbers = generate_unique_numbers(1000)
+# Сортування вставками
+def insertion_sort(array):
+    for i in range(1, len(array)):
+        key = array[i]
+        j = i - 1
+        while j >= 0 and array[j] > key:
+            array[j + 1] = array[j]
+            j -= 1
+        array[j + 1] = key
+    return array
 
-execution_time = timeit.timeit(lambda: tim_sort_sorted(numbers), number = 1)
-print(f"Час виконання 'tim_sort_sorted': {execution_time} секунд")
 
-execution_time = timeit.timeit(lambda: tim_sort_sort(numbers), number = 1)
-print(f"Час виконання 'tim_sort_sort': {execution_time} секунд")
+# Сортування злиттям
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+
+    mid = len(array) // 2
+    left_half = array[:mid]
+    right_half = array[mid:]
+
+    return merge(merge_sort(left_half), merge_sort(right_half))
+
+
+def merge(left, right):
+    merged = []
+    left_index = 0
+    right_index = 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] <= right[right_index]:
+            merged.append(left[left_index])
+            left_index += 1
+        else:
+            merged.append(right[right_index])
+            right_index += 1
+
+    while left_index < len(left):
+        merged.append(left[left_index])
+        left_index += 1
+
+    while right_index < len(right):
+        merged.append(right[right_index])
+        right_index += 1
+
+    return merged
+
+
+def sorting_measurement(len):
+    print(f"Number of items to measure: '{len}'")
+    # Execution time 'Timsort' -> sorted
+    execution_time = timeit.timeit(lambda: timsort_sorted(generate_unique_numbers(len)), number=1)
+    print(f"Execution time 'Timsort -> sorted': {execution_time} seconds")
+    # Execution time 'Timsort' -> sort
+    execution_time = timeit.timeit(lambda: timsort_sort(generate_unique_numbers(len)), number=1)
+    print(f"Execution time 'Timsort -> sort': {execution_time} seconds")
+    # Execution time 'merge'
+    execution_time = timeit.timeit(lambda: merge_sort(generate_unique_numbers(len)), number=1)
+    print(f"Execution time 'merge': {execution_time} seconds")
+    # Execution time 'insertion'
+    execution_time = timeit.timeit(lambda: insertion_sort(generate_unique_numbers(len)), number=1)
+    print(f"Execution time'insertion': {execution_time} seconds")
+
+
+# 1000 elements
+sorting_measurement(1000)
+
+# 10000 elements
+sorting_measurement(10000)
+
+# 100000 elements
+sorting_measurement(100000)
+
+# 1000000 elements
+sorting_measurement(1000000)
